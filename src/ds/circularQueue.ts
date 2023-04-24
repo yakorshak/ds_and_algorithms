52f209c2fad33a4894192b1077059331a7214afe
Andrii Korshak <yakorshak@gmail.com>
2023-04-23 21:11:10 -0400

    Circular Queue

--- src/ds/circularQueue.ts
+++ src/ds/circularQueue.ts
@@ -1,57 +1,63 @@
 class MyCircularQueue {
   queue: number[];
   size: number;
+  head: number;
+  tail: number;
 
   constructor(k: number) {
     this.queue = [];
     this.size = k;
+    this.tail = -1;
+    this.head = -1;
   }
 
+  // Inserts an element into the circular queue. Return true if the operation is successful.
   enQueue(value: number): boolean {
-    if (!this.isFull()) {
-      this.queue.push(value);
-      return true;
-    }
-    return false;
+    if (this.isFull()) return false;
+    if (this.isEmpty()) this.head = 0;
+    this.tail = (this.tail + 1) % this.size;
+    this.queue[this.tail] = value;
+    return true;
   }
 
+  // Deletes an element from the circular queue. Return true if the operation is successful.
   deQueue(): boolean {
-    if (!this.isEmpty()) {
-      this.queue.shift();
+    if (this.isEmpty()) return false;
+    if (this.head == this.tail) {
+      this.head = -1;
+      this.tail = -1;
       return true;
     }
-    return false;
+    this.head = (this.head + 1) % this.size;
+    return true;
   }
 
+  // Gets the front item from the queue. If the queue is empty, return -1.
   Front(): number {
-    if (!this.isEmpty()) {
-      let element = this.queue[0];
-      return element;
-    }
-    return -1;
+    return this.isEmpty() ? -1 : this.queue[this.head];
   }
 
+  // Gets the last item from the queue. If the queue is empty, return -1.
   Rear(): number {
-    if (!this.isEmpty()) {
-      let element = this.queue[this.queue.length - 1];
-      return element;
-    }
-    return -1;
+    return this.isEmpty() ? -1 : this.queue[this.tail];
   }
 
   isEmpty(): boolean {
-    if (this.queue.length === 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return this.head == -1;
   }
 
   isFull(): boolean {
-    if (this.queue.length === this.size) {
-      return true;
-    } else {
-      return false;
-    }
+    return (this.tail + 1) % this.size == this.head;
   }
 }
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * var obj = new MyCircularQueue(k)
+ * var param_1 = obj.enQueue(value)
+ * var param_2 = obj.deQueue()
+ * var param_3 = obj.Front()
+ * var param_4 = obj.Rear()
+ * var param_5 = obj.isEmpty()
+ * var param_6 = obj.isFull()
+ */
